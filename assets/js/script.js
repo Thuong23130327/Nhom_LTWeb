60a9bd19754d04b498cfd78645bd6901c066d014
Thuong23130327 <23130327@st.hcmuaf.edu.vn>
2025-11-18 17:10:23 +0700

    upd checkout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,26 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 
+    // Tìm nút đặt hàng và popup
+    const btnDatHang = document.getElementById('btn-dat-hang');
+    const successPopup = document.getElementById('success-popup');
+
+    if (btnDatHang && successPopup) {
+        btnDatHang.addEventListener('click', function (e) {
+            e.preventDefault(); // Chặn load lại trang
+
+            // 1. Hiện Popup
+            successPopup.classList.add('show');
+
+            // 2. Đợi 2.5 giây rồi chuyển trang
+            setTimeout(function () {
+                // Kiểm tra lại đường dẫn index.html cho đúng
+                // Nếu file checkout nằm trong thư mục con thì dùng "../index.html"
+                window.location.href = "profileM/order-history.html";
+            }, 2000);
+        });
+    }
+
     let mybutton = document.getElementById("back-top-btn");
 
     window.onscroll = function () {
@@ -164,7 +184,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (newsletterForm) {
         // Bắt sự kiện 'submit' của form
-        newsletterForm.addEventListener('submit', function(event) {
+        newsletterForm.addEventListener('submit', function (event) {
             // Ngăn chặn hành vi gửi form mặc định (tránh reload trang)
             event.preventDefault();
 
