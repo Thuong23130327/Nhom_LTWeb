a89c898157096ed6bbb3a334a2d78ad5704a8138
23130238@st.hcmuaf.edu.vn <23130238@st.hcmuaf.edu.vn>
2025-12-06 21:02:37 +0700

    Thêm 1 số class ở nhánh mua bán sản phẩm(Cart, Item, Order)

---
+++ src/main/java/models/Cart.java
@@ -0,0 +1,13 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+    private List<Item> items;
+
+//Constructor
+public Cart() {
+    this.items = new ArrayList<>();
+}
+}
---
+++ src/main/java/models/Order.java
@@ -0,0 +1,10 @@
+package models;
+
+import java.sql.Date;
+
+public class Order {
+    private int id;
+    private int userId;
+    private Date date;
+    private double totalMoney;
+}
