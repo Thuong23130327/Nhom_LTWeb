50d9b88d63f06bf547dd741563b79d962a7bcdf3
23130238@st.hcmuaf.edu.vn <23130238@st.hcmuaf.edu.vn>
2025-12-10 00:56:23 +0700

    Sửa lại cấu trúc order cho đúng với yêu cầu & Tạo class OrderItems

--- src/main/java/model/Order.java
+++ src/main/java/model/Order.java
@@ -1,54 +1,124 @@
 package model;
 
 import java.sql.Date;
+import java.time.LocalDateTime;
 
 public class Order {
     private int id;
-    private int userId;
-    private Date date;
-    private double totalMoney;
+    private Integer usersId;
+    private Integer vouchersId;
+    private String orderCode;
+    private LocalDateTime orderDate;
+    private String status;
+    private String paymentStatus;
+    private double totalProductsPrice;
+    private double shippingFee;
+    private double discountAmount;
+    private double finalAmount;
 
 //Constructor
     public Order() {
     }
 
-    public Order(int id, int userId, Date date, double totalMoney) {
+    public Order(int id, Integer usersId, Integer vouchersId, String orderCode, LocalDateTime orderDate, String status, String paymentStatus, double totalProductsPrice, double shippingFee, double discountAmount, double finalAmount) {
         this.id = id;
-        this.userId = userId;
-        this.date = date;
-        this.totalMoney = totalMoney;
+        this.usersId = usersId;
+        this.vouchersId = vouchersId;
+        this.orderCode = orderCode;
+        this.orderDate = orderDate;
+        this.status = status;
+        this.paymentStatus = paymentStatus;
+        this.totalProductsPrice = totalProductsPrice;
+        this.shippingFee = shippingFee;
+        this.discountAmount = discountAmount;
+        this.finalAmount = finalAmount;
     }
-
- //Getters & Setters
+//Getters & Setters
     public int getId() {
-         return id;
+        return id;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
-    public int getUserId() {
-        return userId;
+    public Integer getUsersId() {
+        return usersId;
     }
 
-    public void setUserId(int userId) {
-        this.userId = userId;
+    public void setUsersId(Integer usersId) {
+        this.usersId = usersId;
     }
 
-    public Date getDate() {
-        return date;
+    public Integer getVouchersId() {
+        return vouchersId;
     }
 
-    public void setDate(Date date) {
-        this.date = date;
+    public void setVouchersId(Integer vouchersId) {
+        this.vouchersId = vouchersId;
     }
 
-    public double getTotalMoney() {
-        return totalMoney;
+    public String getOrderCode() {
+        return orderCode;
     }
 
-    public void setTotalMoney(double totalMoney) {
-        this.totalMoney = totalMoney;
+    public void setOrderCode(String orderCode) {
+        this.orderCode = orderCode;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public double getTotalProductsPrice() {
+        return totalProductsPrice;
+    }
+
+    public void setTotalProductsPrice(double totalProductsPrice) {
+        this.totalProductsPrice = totalProductsPrice;
+    }
+
+    public double getShippingFee() {
+        return shippingFee;
+    }
+
+    public void setShippingFee(double shippingFee) {
+        this.shippingFee = shippingFee;
+    }
+
+    public double getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public void setDiscountAmount(double discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+
+    public double getFinalAmount() {
+        return finalAmount;
+    }
+
+    public void setFinalAmount(double finalAmount) {
+        this.finalAmount = finalAmount;
     }
 }
