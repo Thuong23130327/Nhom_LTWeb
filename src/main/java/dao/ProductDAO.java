74ee66ac316f227d83664829df40520abbaa8450
Tieu Yen <23130397@st.hcmuaf.edu.vn>
2025-12-24 16:26:48 +0700

    sửa jsp hiển thị sản phẩm từ db ra giao diện store.jsp

--- src/main/java/dao/ProductDAO.java
+++ src/main/java/dao/ProductDAO.java
@@ -17,6 +17,42 @@ public class ProductDAO {
         conn = DBConnect.getConnection();
     }
 
+    public List<Product> getAllProduct() {
+        List<Product> list = new ArrayList<>();
+
+        String sql = """
+        SELECT id, sku, name, description, avg_rating, sold_count,
+               brand_id, categories_id, is_active, created_at
+        FROM Products
+    """;
+
+        try (Connection conn = DBConnect.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                Product p = new Product(
+                        rs.getInt("id"),
+                        rs.getString("sku"),
+                        rs.getString("name"),
+                        rs.getString("description"),
+                        rs.getFloat("avg_rating"),
+                        rs.getInt("sold_count"),
+                        rs.getInt("brand_id"),
+                        rs.getInt("categories_id"),
+                        rs.getBoolean("is_active"),
+                        rs.getDate("created_at").toLocalDate()
+                );
+                list.add(p);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+
     public Product getById(int id) {
         String sql = "SELECT * FROM Products WHERE id = ?";
 // Lấy kết nối trực tiếp để tự động trả về Pool
