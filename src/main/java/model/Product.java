83e86c52840b867c73d18c2cb81e6e78f665b8c8
Tieu Yen <23130397@st.hcmuaf.edu.vn>
2025-12-27 22:05:43 +0700

    sửa phương thức doGet và getAllProduct

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -13,6 +13,23 @@ public class Product {
     private int categoriesId;
     private boolean isActive;
     private LocalDate createdAt;
+    private String img;
+    private double price;
+
+    public Product(int id, String sku, String name, String description, float avgRating, int soldCount, int brandId, int categoriesId, boolean isActive, LocalDate createdAt, String img, double price) {
+        this.id = id;
+        this.sku = sku;
+        this.name = name;
+        this.description = description;
+        this.avgRating = avgRating;
+        this.soldCount = soldCount;
+        this.brandId = brandId;
+        this.categoriesId = categoriesId;
+        this.isActive = isActive;
+        this.createdAt = createdAt;
+        this.img = img;
+        this.price = price;
+    }
 
     public Product(int id, String sku, String name, String description, float avgRating, int soldCount, int brandId,
                    int categoriesId, boolean isActive, LocalDate createdAt) {
@@ -109,7 +126,19 @@ public class Product {
         this.createdAt = createdAt;
     }
 
+    public String getImg() {
+        return img;
+    }
 
+    public void setImg(String img) {
+        this.img = img;
+    }
 
+    public double getPrice() {
+        return price;
+    }
 
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
